1aacbe554d0fa36952f1be26324b04a3db838d3d
Micaela Jawor <micaelajawor@yahoo.com>
2019-02-21 13:13:06 -0800

    update controller page

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace ToDoList.Controllers
   public class HomeController : Controller
   {
 
-    [Route("/")]
+    [HttpGet("/")]
     public ActionResult Index()
     {
       Item starterItem = new Item("Add first item to To Do List");
@@ -19,7 +19,7 @@ namespace ToDoList.Controllers
       return View();
     }
 
-    [Route("/items")]
+    [HttpPost("/items")]
     public ActionResult Create(string description)
     {
       Item myItem = new Item(description);
