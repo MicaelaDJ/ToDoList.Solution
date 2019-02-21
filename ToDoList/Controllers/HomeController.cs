ee7662a0b1779a9339f4a1a7aadd56c7ac2f6c60
Micaela Jawor <micaelajawor@yahoo.com>
2019-02-21 14:14:54 -0800

    update itemscontroller and homecontroller files

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
@@ -10,22 +8,8 @@ namespace ToDoList.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
-
-    [Route("/items/new")]
-    public ActionResult CreateForm()
-    {
       return View();
     }
-
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-    }
-
+    
   }
 }
