c4be1c43e56511610226687fa5a95d8fc3eee652
Micaela Jawor <micaelajawor@yahoo.com>
2019-02-26 11:45:42 -0800

    work on categories

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -16,5 +16,35 @@ namespace ToDoList.Models
       _id = _instances.Count;
       _items = new List<Item>{};
     }
+
+    public string GetName()
+    {
+      return _name;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static List<Category> GetAll()
+    {
+      return _instances;
+    }
+    public static Category Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+    public List<Item> GetItems()
+    {
+      return _items;
+    }
+    public void AddItem(Item item)
+    {
+      _items.Add(item);
+    }
+
   }
 }
