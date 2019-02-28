a36ffe5dade89a0547767bd12b14255957a3676b
Micaela Jawor <micaelajawor@yahoo.com>
2019-02-28 13:17:34 -0800

    update item.cs find() method

--- ToDoList.Tests/ControllerTests/ItemsControllerTests.cs
+++ ToDoList.Tests/ControllerTests/ItemsControllerTests.cs
@@ -10,30 +10,30 @@ namespace ToDoList.Tests
   public class ItemControllerTest
   {
 
-    //test to check the datatype of the IActionResult returned from the action
-    [TestMethod]
-    public void Create_ReturnsCorrectActionType_RedirectToActionResult()
-    {
-      //Arrange
-      ItemsController controller = new ItemsController();
-      //Act
-      IActionResult view = controller.Create("Walk the dog");
-      //Assert
-      Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
-    }
+    // //test to check the datatype of the IActionResult returned from the action
+    // [TestMethod]
+    // public void Create_ReturnsCorrectActionType_RedirectToActionResult()
+    // {
+    //   //Arrange
+    //   ItemsController controller = new ItemsController();
+    //   //Act
+    //   IActionResult view = controller.Create("Walk the dog");
+    //   //Assert
+    //   Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
+    // }
+    //
+    // //test checks the name of the action that we redirect to
+    // [TestMethod]
+    // public void Create_RedirectsToCorrectAction_Index()
+    // {
+    //   //Arrange
+    //   ItemsController controller = new ItemsController();
+    //   RedirectToActionResult actionResult = controller.Create("Walk the dog") as RedirectToActionResult;
+    //   //Act
+    //   string result = actionResult.ActionName;
+    //   //Assert
+    //   Assert.AreEqual(result, "Index");
+    // }
 
-    //test checks the name of the action that we redirect to
-    [TestMethod]
-    public void Create_RedirectsToCorrectAction_Index()
-    {
-      //Arrange
-      ItemsController controller = new ItemsController();
-      RedirectToActionResult actionResult = controller.Create("Walk the dog") as RedirectToActionResult;
-      //Act
-      string result = actionResult.ActionName;
-      //Assert
-      Assert.AreEqual(result, "Index");
-    }
-    
   }
 }
